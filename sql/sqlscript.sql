feaaad706612ac3c4ed4bc9dddbccc7402c75c85
Priyansh <saxenapriyansh95@gmail.com>
2026-09-04 07:39:05 +0530

    updated SQL script

--- sql/sqlscript.sql
+++ sql/sqlscript.sql
@@ -5,3 +5,7 @@ CREATE TABLE btc AS SELECT * FROM read_csv_auto('btc_daily.csv')
 """)
 
 con.sql("SELECT * FROM btc LIMIT 5").show()
+
+-- checking for NULLs
+con.sql("SELECT * FROM btc WHERE date is NULL OR close IS NULL OR high IS NULL OR low IS NULL OR
+    open IS NULL or vol IS NULL").show()
