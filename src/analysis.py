a641412b4930fa52abdd0d267b48a01fc5951660
Priyansh <saxenapriyansh95@gmail.com>
2026-09-04 23:48:57 +0530

    added the python script

--- src/analysis.py
+++ src/analysis.py
@@ -2,4 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd 
 import statsmodels as sm
+import duckdb 
 
+con = duckdb.connect('home/priyansh/Documents/d/btc bull vs bear regimes/data/raw/main.duckdb')
+
+df = con.sql("""
+    SELECT regime, 
+    AVG(daily_returns) AS avg_daily_return,
+    STDDEV(daily_returns) AS volatility
+    FROM btc_regime_updated 
+    WHERE regime IS NOT NULL 
+    GROUP BY regime 
+    """).df()
+
+print(df.head(10))
