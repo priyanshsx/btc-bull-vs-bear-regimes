12bebb0c643eb759c3874011e82a5fa3648b3add
Priyansh <saxenapriyansh95@gmail.com>
2026-09-04 07:56:44 +0530

    added the btc_regime table

--- sql/sqlscript.sql
+++ sql/sqlscript.sql
@@ -9,3 +9,19 @@ con.sql("SELECT * FROM btc LIMIT 5").show()
 -- checking for NULLs
 con.sql("SELECT * FROM btc WHERE date is NULL OR close IS NULL OR high IS NULL OR low IS NULL OR
     open IS NULL or vol IS NULL").show()
+
+-- using ma 200 for classification 
+
+con.sql("""
+CREATE TABLE btc_regime AS
+SELECT 
+    date,
+    close,
+    AVG(close) OVER (ORDER BY date ROWS BETWEEN 199 PRECEDING AND CURRENT ROW) AS ma_200,
+    CASE
+        WHEN close > AVG(close) OVER (ORDER BY date ROWS BETWEEN 199 PRECEDING AND CURRENT ROW) THEN 'bull'
+        ELSE 'bear'
+    END AS regime
+FROM btc
+ORDER BY date
+""")
