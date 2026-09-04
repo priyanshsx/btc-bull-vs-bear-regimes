6a19a6ee9b472887eeb0511241eea293f93d8a8c
Priyansh <saxenapriyansh95@gmail.com>
2026-09-04 11:50:05 +0530

    formatting changes in sql script

--- sql/sqlscript.sql
+++ sql/sqlscript.sql
@@ -1,18 +1,17 @@
 -- creating the btc table 
 
-con.sql("""
 CREATE TABLE btc AS SELECT * FROM read_csv_auto('btc_daily.csv')
-""")
 
-con.sql("SELECT * FROM btc LIMIT 5").show()
+
+SELECT * FROM btc LIMIT 5
 
 -- checking for NULLs
-con.sql("SELECT * FROM btc WHERE date is NULL OR close IS NULL OR high IS NULL OR low IS NULL OR
-    open IS NULL or vol IS NULL").show()
+SELECT * FROM btc WHERE date is NULL OR close IS NULL OR high IS NULL OR low IS NULL OR
+    open IS NULL or vol IS NULL
 
 -- using ma 200 for classification 
 
-con.sql("""
+
 CREATE TABLE btc_regime AS
 SELECT 
     date,
@@ -24,10 +23,10 @@ SELECT
     END AS regime
 FROM btc
 ORDER BY date
-""")
+
 
 -- checking for NULLs
-con.sql("SELECT * FROM btc_regime WHERE date IS NULL OR close IS NULL OR ma_200 IS NULL OR regime IS NULL").show()
+SELECT * FROM btc_regime WHERE date IS NULL OR close IS NULL OR ma_200 IS NULL OR regime IS NULL
 
 -- I was expecting the first 199 entries to show up as NULL, which did not happen. This may be because SQL is taking the average of the current row (since we have in our code ROWS BETWEEN 199 PRECEDING AND CURRENT ROW).
 -- Since those are false values and do not amount to 200-Day MA, the first 199 rows will have to be NULLed out. 
