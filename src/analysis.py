e834a0fec79930fecbe78c47e90e7074a6aa26c9
Priyansh <saxenapriyansh95@gmail.com>
2026-09-07 12:08:45 +0530

    ran the t-test on the two bull and bear samples

--- src/analysis.py
+++ src/analysis.py
@@ -1,8 +1,9 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import pandas as pd 
-import statsmodels as sm
+from scipy import stats
 import duckdb 
+
 
 con = duckdb.connect('/home/priyansh/Documents/d/btc bull vs bear regimes/data/raw/main.duckdb')
 
@@ -14,4 +15,25 @@ df = con.sql("""
     WHERE regime IS NOT NULL 
     GROUP BY regime 
     """).df()
-print(df.head(10))
+# print(df.head(10))
+
+# pulling the bull regime and creating a new dedicated df
+
+df_bull = con.sql("""
+    SELECT daily_returns
+    FROM btc_regime_updated
+    WHERE regime = 'bull'
+""").df()['daily_returns']
+
+# pulling the bear regime and creating a new dedicated df
+
+df_bear = con.sql("""
+    SELECT daily_returns
+    FROM btc_regime_updated 
+    WHERE regime = 'bear'
+""").df()['daily_returns']
+
+# calculating the t-stat 
+t_stat, p_value = stats.ttest_ind(df_bull, df_bear)
+print(f"T-statistic: {t_stat: .4f}")
+print(f"p_value: {p_value: .4f}")
